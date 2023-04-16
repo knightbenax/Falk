fa8f31f974b248c88583238d9d65aa121372c1c8
Bezaleel Ashefor <knightbenax@gmail.com>
2023-04-16 22:37:52 +0100

    Initial Commit

---
+++ Falk/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Falk
+//
+//  Created by Bezaleel Ashefor on 16/04/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
